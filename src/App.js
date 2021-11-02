fab2d1d979e3b8f062cd9f12de4230d968c1b28f
EngineeringDeveloper <ryan.c.parki@gmail.com>
2021-11-02 22:36:06 +0000

    Changed to CSS styles for ease of use

--- src/App.js
+++ src/App.js
@@ -2,32 +2,37 @@ import React, { useState } from 'react';
 
 // Dark & Light Themes
 // https://www.smashingmagazine.com/2020/04/dark-mode-react-apps-styled-components/#globalstyles-component
-import {ThemeProvider} from "styled-components";
-import { GlobalStyles } from "./Components/globalStyles";
-import { lightTheme, darkTheme } from "./Components/Themes"
+// import {ThemeProvider} from "styled-components";
+// import { GlobalStyles } from "./Components/globalStyles";
+// import { lightTheme, darkTheme } from "./Components/Themes"
+
+// Theme
+import ThemeToggler from './Components/ThemeToggler';
+import  {useDarkMode} from "./Components/useDarkMode"
+// https://medium.com/swlh/setting-up-light-and-dark-mode-in-a-react-application-just-with-styles-7790dea22aed
+import './styles/theme.css';
 
 // Custom Components
-import Sidebar from './Components/Sidebar'
-
+import Sidebar from './Components/Sidebar/Sidebar'
 
 
 function App() {
-  const [theme, setTheme] = useState('light');
-  const themeToggler = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light')
-}
+  // Theme toggling
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+  // const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
+  if(!mountedComponent) return <div/>
   return(
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <>
-      <GlobalStyles/>
-    <div>
-      {/* <Sidebar /> */}
-      <button onClick={themeToggler}>Switch Theme</button>
+    <div className={`App ${theme}`}>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        {/* Theme toggling Button  */}
+        <ThemeToggler theme={theme} toggleTheme={themeToggler}/>
+      </div>
     </div>
-    </>
-    </ThemeProvider>
-  )
+    )
 }
 
 export default App;
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import '../App.css';
+import './Sidebar.css';
+// import styled from "styled-components"
+
 
 // https://www.youtube.com/watch?v=5R9jFHlG6ik&ab_channel=PedroTech
 function Sidebar() {
