77da1c99d6f04c5f74b1bfeb7bab4d6de8b37443
EngineeringDeveloper <ryan.c.parki@gmail.com>
2021-11-19 19:18:59 +0000

    More Fiddling with toggling theme

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import React from 'react';
 // import { lightTheme, darkTheme } from "./Components/Themes"
 
 // Theme
-import  {useDarkMode} from "./Components/useDarkMode"
+import { useDarkMode } from "./Components/useDarkMode"
+import { ThemeProvider } from 'styled-components';
 // https://medium.com/swlh/setting-up-light-and-dark-mode-in-a-react-application-just-with-styles-7790dea22aed
 import './styles/theme.css';
 import './App.css'
@@ -19,24 +20,26 @@ import './App.css'
 import Sidebar from './Components/Sidebar/Sidebar'
 import Taskview from './Components/Taskview/Taskview'
 import NewToDoModal from './Components/NewToDoModal/NewToDoModal'
+import { GlobalStyles } from '@mui/styled-engine';
 
 function App() {
   // Theme toggling
   const [theme, themeToggler, mountedComponent] = useDarkMode();
   // const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
-  if(!mountedComponent) return <div/>
-  return(
-    <div className={`App ${theme}`}>
-      <div className="Row">
-        <Sidebar theme={theme} toggleTheme={themeToggler} />
-        {/*Pass down theme and toggler to component that needs it */}
-          <Taskview />
-            {/* Theme toggling Button  */}
-        {/* <ThemeToggler theme={theme} toggleTheme={themeToggler}/> */}
-          <NewToDoModal />
-      </div>
-    </div>
+  if (!mountedComponent) return <div />
+  
+  return (
+      <div className={`App ${theme}`}>
+        <div className="Row">
+          <Sidebar theme={theme} toggleTheme={themeToggler} />
+          {/*Pass down theme and toggler to component that needs it */}
+            <Taskview />
+              {/* Theme toggling Button  */}
+          {/* <ThemeToggler theme={theme} toggleTheme={themeToggler}/> */}
+            <NewToDoModal />
+        </div>
+        </div>
     )
 }
 
