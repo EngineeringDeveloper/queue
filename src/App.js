335bae70a6d7ec98f9bbba4f9643a22371d9b012
EngineeringDeveloper <ryan.c.parki@gmail.com>
2021-11-21 22:07:35 +0000

    Trying to get tasks to display on Taskview put a spinny in while its not working

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import NewToDoModal from './Components/NewToDoModal/NewToDoModal'
 // Tauri Commands
 // https://tauri.studio/en/docs/usage/guides/command
 // With the Tauri API npm package:
-import { invoke } from '@tauri-apps/api/tauri'
+// import { invoke } from '@tauri-apps/api/tauri'
 // With the Tauri global script, enabled when `tauri.conf.json > build > withGlobalTauri` is set to true:
 
 
--- src/Components/Taskview/Taskview.js
+++ src/Components/Taskview/Taskview.js
@@ -6,31 +6,40 @@ import { invoke } from '@tauri-apps/api/tauri'
 
 function Taskview  (){
     const [input, setInput] = useState(''); // '' is the initial state value
-    invoke("parse_todo", { path: "path" }).then(taskList) => console.log(taskList);
-// https://stackoverflow.com/questions/36683770/how-to-get-the-value-of-an-input-field-using-reactjs
+    // invoke("parse_todo", { path: "path" }).then((taskList) => taskList.map((key, val) => console.log(key)));
+    // const taskList = invoke("parse_todo", { path: "path" }).then((taskList) => { return taskList });
+    // console.log(taskList)
+    // https://stackoverflow.com/questions/36683770/how-to-get-the-value-of-an-input-field-using-reactjs
         return (
             <div className="Taskview">
                 <header className="Header">
-                    {/* <div></div> */}
-                        {/* <div> */}
                         <SearchIcon />
                         <input className="TaskviewSearch" type="search" value={input} onInput={e => setInput(e.target.value)}
                         placeholder="(A) Todo text +project @context due:2020-12-12 rec:d"/>
-                        {/* </div> */}
                 </header>
                 <div className="Content">
                     <p>"Content"</p>
-                    {taskList.map((key, val) => {
-                        return (
-                            <li key={key, val}
-                                className="row">
-                                {" "}
-                                <div id="title">
-                                    {val}
+                    <div id = "icon" className="Loading">
+                        <SearchIcon />
+                    </div>
+                    {/* <li key={0}
+                        className="row">
+                        {fat[0].test}
+                    </li> */}
+                    {/* {invoke("parse_todo", { path: "path" }).then((taskList) => {
+                        return taskList.map((value, key) => {
+                            // console.log(key, value.subject)
+                            return (
+                                <li key={key}
+                                    className="row">
+                                    {value.subject}
+                                    <div id="content">
+                                    {value.subject}
                                 </div>
-                            </li>
-                        )
-                    })}
+                                </li>
+                            )
+                        })
+                    })} */}
                 </div>
             </div>
         );
