a6757e1d515ac37b2fef032568f20a215cc859ff
EngineeringDeveloper <ryan.c.parki@gmail.com>
2021-11-23 20:55:54 +0000

    Tasklist is sorted before display

--- src/Components/Taskview/Task.js
+++ src/Components/Taskview/Task.js
@@ -1,11 +1,56 @@
-import React from 'react'
-import Checkbox from '@mui/material/Checkbox';
+import React from "react";
+import Checkbox from "@mui/material/Checkbox";
+
+// pub struct Simple {
+//     pub subject: String,
+//     #[cfg_attr(feature = "serde-support", serde(default = "Priority::lowest"))]
+//     pub priority: u8,
+//     pub create_date: Option<crate::Date>,
+//     pub finish_date: Option<crate::Date>,
+//     #[cfg_attr(feature = "serde-support", serde(default))]
+//     pub finished: bool,
+//     pub threshold_date: Option<crate::Date>,
+//     pub due_date: Option<crate::Date>,
+//     #[cfg_attr(feature = "serde-support", serde(default))]
+//     pub contexts: Vec<String>,
+//     #[cfg_attr(feature = "serde-support", serde(default))]
+//     pub projects: Vec<String>,
+//     #[cfg_attr(feature = "serde-support", serde(default))]
+//     pub hashtags: Vec<String>,
+//     #[cfg_attr(feature = "serde-support", serde(default))]
+//     pub tags: BTreeMap<String, String>,
+// }
 
 export default function Task(props) {
-    return (
-        <li key={props.key}className="Task">
-            <Checkbox></Checkbox>
-            <div>{props.details.subject}</div>
-        </li>
-    )
+  console.log(typeof props.details.tags);
+//   console.log(typeof props.details.priority);
+  return (
+    <li key={props.details.priority} className='Task'>
+      <Checkbox></Checkbox>
+      {/* <div>{"priority :"}</div> */}
+      <div>{props.details.priority}</div>
+      <div>{"subject :"}</div>
+      <div>{props.details.subject}</div>
+      <div>{"Project :"}</div>
+      <div>{props.details.projects}</div>
+      <div>
+        <div>{"date :"}</div>
+        <div>{props.details.create_date}</div>
+        <div>{"date_f :"}</div>
+        <div>{props.details.finish_date}</div>
+        <div>{"fin :"}</div>
+        <div>{props.details.finished}</div>
+        <div>{"due :"}</div>
+        <div>{props.details.due_date}</div>
+        <div>{"contexts :"}</div>
+        <div>{props.details.contexts}</div>
+        <div>{"hash :"}</div>
+        <div>{props.details.hashtags}</div>
+        <div>{"tags :"}</div>
+        {/* {props.details.tags.map((value, key) => {
+                    return (<div> value </div>)
+            })} */}
+      </div>
+    </li>
+  );
 }
--- src/Components/Taskview/Taskview.js
+++ src/Components/Taskview/Taskview.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Task from "./Task.js"
+import Task from "./Task.js";
 import "./Taskview.css";
 import SearchIcon from "@mui/icons-material/Search";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 // https://mui.com/components/progress/
 import { invoke } from "@tauri-apps/api/tauri";
 
@@ -17,54 +17,53 @@ class Taskview extends Component {
       error: null,
       loading: true,
     };
-    }
+  }
 
-    componentDidMount() {
-      // If the component mounted then we evaluate if the promise resolved
+  componentDidMount() {
+    // If the component mounted then we evaluate if the promise resolved
     this.state.taskList
-        .then((list) => {
-        this.setState({ taskList: list.map((value, key) => {
-            return (
-                <Task details={value} key={key}></Task>
-            );
-        })});
-    })
-    .finally(() => {
-        this.setState({ loading: false } );
-    });
+      .then((list) => {
+        let sortedList = list.sort((a, b) => a.priority - b.priority);
+        this.setState({
+          taskList: sortedList.map((value, key) => {
+            return <Task details={value} key={key}></Task>;
+          }),
+        });
+      })
+      .finally(() => {
+        this.setState({ loading: false });
+      });
   }
 
   // https://stackoverflow.com/questions/36683770/how-to-get-the-value-of-an-input-field-using-reactjs
-    render() {
-        let LoadedTasks;
-        if (this.state.loading) {
-            console.log(this.state.loading);
-            LoadedTasks = <box id="icon" ><CircularProgress color="inherit"/></box>;
-        } else {
-            console.log(this.state)
-            LoadedTasks = this.state.taskList
-        }
-    
-        return (
-          <div className='Taskview'>
-            <header className='Header'>
-              <SearchIcon />
-              <input
-                className='TaskviewSearch'
-                type='search'
-                value={this.input}
-                onInput={(e) => (this.input = e.target.value)}
-                placeholder='(A) Todo text +project @context due:2020-12-12 rec:d'
-              />
-            </header>
-            <ul className='Content'>
-                    {LoadedTasks}
-            </ul>
-          </div>
-        );
+  render() {
+    let LoadedTasks;
+    if (this.state.loading) {
+      LoadedTasks = (
+        <box id='icon'>
+          <CircularProgress color='inherit' />
+        </box>
+      );
+    } else {
+      LoadedTasks = this.state.taskList;
+    }
+
+    return (
+      <div className='Taskview'>
+        <header className='Header'>
+          <SearchIcon />
+          <input
+            className='TaskviewSearch'
+            type='search'
+            value={this.input}
+            onInput={(e) => (this.input = e.target.value)}
+            placeholder='(A) Todo text +project @context due:2020-12-12 rec:d'
+          />
+        </header>
+        <ul className='Content'>{LoadedTasks}</ul>
+      </div>
+    );
   }
 }
-
-
 
 export default Taskview;
