ffc6334ffc0ac705b4d8f41a00075e944bd74305
EngineeringDeveloper <ryan.c.parki@gmail.com>
2021-12-22 19:17:39 +0000

    source

--- src/Components/NewToDoModal/NewToDoModal.js
+++ src/Components/NewToDoModal/NewToDoModal.js
@@ -14,7 +14,7 @@ export default function NewToDoModal(props) {
       document.body.removeEventListener("keydown", closeOnEscapeDown);
     };
   });
-
+//     https://medium.com/tinyso/how-to-create-a-modal-component-in-react-from-basic-to-advanced-a3357a2a716a
   // if (!props.show) {
   //     return null
   // }
