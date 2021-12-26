82749cf55cba553cdcfa935fd937977d90022231
EngineeringDeveloper <ryan.c.parki@gmail.com>
2021-12-26 15:12:48 +0000

    No longer display Date values that dont exist tying to set formatting for dates to spaced

--- src/Components/Taskview/TaskComponents/Date.js
+++ src/Components/Taskview/TaskComponents/Date.js
@@ -1,17 +1,45 @@
-import React from 'react'
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import "./Date.css"
+import React from "react";
+import CalendarTodayTwoToneIcon from "@mui/icons-material/CalendarTodayTwoTone";
+import "./Date.css";
 
 function Date(props) {
+  const NotNull = (element) => element !== null;
+  
+  let Created;
+  if (NotNull(props.task.create_date)) {
+    Created = <div>{"Created :" + props.task.create_date}</div>;
+  } else {
+    Created = null;
+  }
+  console.log("Created", props.task.create_date, NotNull(props.task.create_date), Created)
+  let Finished;
+  if (NotNull(props.task.finish_date)) {
+    Finished = <div>{"Finished :" + props.task.finish_date}</div>;
+  } else {
+    Finished = null;
+  }
+
+  let Due;
+  if (NotNull(props.task.due_date)) {
+    Due = <div>{"Due :" + props.task.due_date}</div>;
+  } else {
+    Due = null;
+  }
+
+  if ([Created, Finished, Due].some(NotNull)) {
     return (
       <div className='Dates'>
-          <CalendarTodayIcon></CalendarTodayIcon>
-          <div>{"Created :" + props.task.create_date}</div>
-          <div>{"Finished :" + props.task.finish_date}</div>
-          {/* <div>{"fin :" + props.task.finished}</div> */}
-          <div>{"Due :" + props.task.due_date}</div>
-        </div>
-    )
+        <CalendarTodayTwoToneIcon></CalendarTodayTwoToneIcon>
+        {Created}
+        {Finished}
+        {Due}
+      </div>
+    );
+    
+  } else {
+    return (null);
+  }
+
 }
 
-export default Date
+export default Date;
