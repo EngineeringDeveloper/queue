68db24297646cb9f6d0c3a3c668a317426f98ca7
EngineeringDeveloper <ryan.c.parki@gmail.com>
2021-12-26 17:16:46 +0000

    Get TaskDetails in modal but bad formatting

--- src/Components/Modal/NewToDoModal.js
+++ src/Components/Modal/NewToDoModal.js
@@ -1,18 +1,26 @@
 import React from "react";
 import "./NewToDoModal.css";
 import Modal from "./Modal.js";
+import Boxed from "../Taskview/TaskComponents/Boxed.js"
 
 export default function NewToDoModal(props) {
-  const title = "no title";
 
   const placeholder = "(A) Todo text +project @context due:2020-12-12 rec:d";
   var inputtext = null;
+  var taskDetails = null;
   if (props.task) {
     inputtext = props.task.subject;
+    taskDetails = <div style={{ padding: 0, display: "flex", flexFlow: "row"}}>
+    <Boxed array={props.task.projects} cssClass='Project' />
+    <Boxed array={props.task.contexts} cssClass='Context' />
+    <Boxed array = {props.task.hashtags} cssClass="Hashtag"/>
+  </div>
   }
+
   const children =
     <div className="content">
-      <input className="textInput" type="text" placeholder={placeholder} />
+      <input className="textInput" type="text" placeholder={placeholder} value={inputtext}/>
+      {taskDetails}
     </div>
 
   const submit = (content) => {
